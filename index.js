c29bb2ec144d53d2fbf5df7bd734385f4b14d386
Pravin singh <>
2023-03-28 06:31:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// let name="Harry"
+// let last_Name="Potter"
+// let sentence=`${name} your last \"name" \n is ${last_Name}`
+// console.log(sentence);
+
+
+// // string menupulation
+// let name ="harry";
+// let name2="JOHN"
+// console.log(name.toUpperCase());
+// console.log(name2.toLowerCase())
+// console.log(name.slice(2,4));
+// console.log(name.slice(2));
+// console.log(name.replace("harry","HARI"));
+// console.log(name.concat(" is friend of ", name2 , " ok "));
+
+
+// Array Menuplation
+let arr =[2,4,5,3,"RAHUL"];
+// console.log(arr);
+// let arr2=arr.toString();
+// console.log(arr2)
+// join method
+// let arr3=arr.join("+");
+// console.log(arr3);
+// removes last element using pop function
+// arr.pop()
+// console.log(arr);
+// add to last element using pop function
+// arr.push(28)
+// console.log(arr);
+// remove to first element
+// arr.shift()
+// console.log(arr);
+// add to first element
+// arr.unshift(28)
+// console.log(arr);
+
+// delete array methods
+// delete arr[0];
+// console.log(arr);
+// concatination in array
+let num1=[1,2,69,9,5,56,4];
+// let new_arr=arr.concat(num1);
+// console.log(new_arr);
+
+// sorting thr array
+// num1.sort();
+// console.log(num1);
+
+// // using function acending order
+// let compare=(a,b)=>{
+//   return a-b
+// }
+// num1.sort(compare);
+// console.log(num1);
+
+// reverse original array
+// num1.reverse();
+// console.log(num1)
+
+// array splice modifies the original array
+
+num1.splice(2,3,1021,2312,200)
+
+
+
